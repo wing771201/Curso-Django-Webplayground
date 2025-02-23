1b8a988955958cda9eef792e6de43b564ef4d555
wing771201 <garciacruz771@gmail.com>
2025-02-23 13:46:10 -0700

    Registro CBV

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Paths de Auth
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
 ]
